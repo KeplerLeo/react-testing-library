b9373f6d30dc15788839f79be8cd525d46be52e2
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-09-19 16:57:24 -0300

    removendo import não usado :hammer:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,3 @@
-import react from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
