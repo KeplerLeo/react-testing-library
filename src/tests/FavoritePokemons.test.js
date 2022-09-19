9ad88bea802b723d41eca6330cd2354cc53b901b
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-09-19 16:05:50 -0300

    requisito 3 :white_check_mark:

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('', () => {
+  const { history } = renderWithRouter(<App />);
+  const favPage = '/favorites';
+  const pikachuUrl = '/pokemons/25';
+
+  act(() => {
+    history.push(favPage);
+  });
+  const notFoundMessage = screen.getByText(/No favorite pokemon found/i);
+  expect(notFoundMessage).toBeInTheDocument();
+
+  act(() => {
+    history.push(pikachuUrl);
+  });
+
+  const favPokemon = screen.getByText('Pokémon favoritado?');
+  userEvent.click(favPokemon);
+
+  act(() => {
+    history.push(favPage);
+  });
+
+  const pikachuName = screen.getByText('Pikachu');
+  expect(pikachuName).toBeInTheDocument();
+});
